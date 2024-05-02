2ce1aa0552408f31f2e2bccbe3f27decd3b517cd
jessochs <fac17002@byui.edu>
2024-05-02 16:45:02 -0700

    getting the layouts and partials

--- server.js
+++ server.js
@@ -5,15 +5,28 @@
 /* ***********************
  * Require Statements
  *************************/
-const express = require("express")
+const express = require("express") 
+const expressLayouts = require("express-ejs-layouts");
 const env = require("dotenv").config()
 const app = express()
 const static = require("./routes/static")
+// Index route
+app.get("/", function(req, res) {
+  res.render("index", {title: "Home"})
+})
 
 /* ***********************
  * Routes
  *************************/
 app.use(static)
+
+/* ***********************
+ * View Engine and Templates
+ *************************/
+app.set("view engine", "ejs")
+app.use(expressLayouts)
+app.set("layout", "./layouts/layout")
+
 
 /* ***********************
  * Local Server Information

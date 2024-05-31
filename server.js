c86fa28fc92b09424a5b2a52ad3e38838f0e0dbf
jessochs <fac17002@byui.edu>
2024-05-30 19:10:54 -0700

    activity 2 2/2

--- server.js
+++ server.js
@@ -12,6 +12,7 @@ const app = express()
 const static = require("./routes/static")
 const baseController = require("./controllers/baseController");
 const inventoryRoute = require("./routes/inventoryRoute");
+const utilities = require("./utilities/");
 
 /* ***********************
  * View Engine and Templates
@@ -28,7 +29,7 @@ app.use(static)
 
 
 // Index route
-app.get("/", baseController.buildHome); 
+app.get("/", utilities.handleErrors(baseController.buildHome)); 
 
 // Inventory routes
 app.use("/inv", inventoryRoute);
@@ -45,9 +46,10 @@ app.use(async (req, res, next) => {
 app.use(async (err, req, res, next) => {
   let nav = await utilities.getNav()
   console.error(`Error at: "${req.originalUrl}": ${err.message}`)
+  if(err.status == 404){ message = err.message} else {message = 'Oh no! There was a crash. Maybe try a different route?'}
   res.render("errors/error", {
     title: err.status || 'Server Error',
-    message: err.message,
+    message,
     nav
   })
 });

470c6ad39f466d643df16d0e9d0222aa22e1a528
jessochs <fac17002@byui.edu>
2024-06-27 15:58:59 -0700

    login validation and login process

--- server.js
+++ server.js
@@ -17,6 +17,7 @@ const utilities = require("./utilities/");
 const session = require("express-session");
 const pool = require('./database/');
 const bodyParser = require("body-parser")
+const cookieParser = require("cookie-parser");
 
 /* ***********************
  * Middleware
@@ -31,6 +32,9 @@ app.use(session({
   saveUninitialized: true,
   name: 'sessionId',
 }))
+app.use(cookieParser())
+
+app.use(utilities.checkJWTToken)
 
 // Express Messages Middleware
 app.use(require('connect-flash')())
--- utilities/account-validation.js
+++ utilities/account-validation.js
@@ -3,6 +3,32 @@ const { body, validationResult } = require("express-validator")
 const accountModel = require("../models/account-model")
 const validate = {}
 
+/*  **********************************
+  *  Registration Data Validation Rules
+  * ********************************* */
+validate.loginRules = () => {
+  return [
+    body("account_email")
+    .trim()
+    .isEmail()
+    .normalizeEmail()
+    .withMessage("Please provide a valid email"),
+
+    body("account_password")
+      .trim()
+      .isStrongPassword({
+        minLength: 12,
+        minLowercase: 1,
+        minUppercase: 1,
+        minNumbers: 1,
+        minSymbols: 1,
+      })
+      .withMessage("Password does not meet requirements.")
+    
+  ]
+}
+
+
 
 /*  **********************************
   *  Registration Data Validation Rules
@@ -74,6 +100,28 @@ validate.checkRegData = async (req, res, next) => {
     }
     next()
   }
+
+    /* ******************************
+ * Check data and return errors or continue to registration
+ * ***************************** */
+validate.checkLoginData = async (req, res, next) => {
+  const {account_email, account_password} = req.body
+  let errors = []
+  errors = validationResult(req)
+
+  if (!errors.isEmpty()) {
+    let nav = await utilities.getNav()
+    res.render("account/login", {
+      errors,
+      title: "Login",
+      nav,
+      account_email,
+      account_password,
+    })
+    return
+  }
+  next()
+}
   
   module.exports = validate
   

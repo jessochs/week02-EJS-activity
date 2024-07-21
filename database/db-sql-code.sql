216a77271f4ae5b8cc95960f3e98410727b2ecef
jessochs <fac17002@byui.edu>
2024-07-20 18:53:54 -0700

    changed a few things and rebuilt db

--- database/db-sql-code.sql
+++ database/db-sql-code.sql
@@ -6,7 +6,7 @@ CREATE TYPE public.account_type AS ENUM
     ('Client', 'Employee', 'Admin');
 
 ALTER TYPE public.account_type
-    OWNER TO db340;
+    OWNER TO db2340;
 
 -- Table structure for table 'classification'
 CREATE TABLE public.classification (
